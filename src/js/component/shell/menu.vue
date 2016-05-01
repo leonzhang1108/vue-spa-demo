a526eb857682d7cf6d65ac9b9d91c9b05575f306
leonzhang <human1108@163.com>
2016-05-01 13:43:27 +0800

    current menu

--- src/js/component/shell/menu.vue
+++ src/js/component/shell/menu.vue
@@ -3,8 +3,9 @@
         <div class="pure-menu">
             <a class="pure-menu-heading" href="#">VUE - DEMO</a>
             <ul class="pure-menu-list">
-                <li class="pure-menu-item" v-for="menu in menus">
-                    <a class="pure-menu-link" v-link="{ path: menu.path }" @click="clickToCurrent(this)">{{menu.name}}</a>
+                <li class="pure-menu-item" v-bind:class="{'menu_current': menu.current}" v-for="menu in menus">
+                    <a class="pure-menu-link" v-link="{ path: menu.path }" @click="clickToCurrent">{{menu.name}}</a>
+                    <span class="trangle"></span>
                 </li>
             </ul>
         </div>
@@ -13,32 +14,48 @@
 
 <script>
     module.exports = {
-        data: function(){
+        data: function () {
             return {
                 menus: [{
-                    path:'/services',
-                    name:'货物状态'
-                },{
-                    path:'/home',
-                    name:'货物详情'
-                },{
-                    path:'/about',
-                    name:'账单制作'
-                },{
-                    path:'/calender',
-                    name:'账单详情'
-                },{
-                    path:'/test',
-                    name:'场站物料管理'
+                    path: '/services',
+                    name: '货物状态',
+                    current: false
+                }, {
+                    path: '/home',
+                    name: '货物详情',
+                    current: false
+                }, {
+                    path: '/about',
+                    name: '账单制作',
+                    current: false
+                }, {
+                    path: '/calender',
+                    name: '账单详情',
+                    current: false
+                }, {
+                    path: '/test',
+                    name: '场站物料管理',
+                    current: false
                 }, {
                     path: '/table',
-                    name: '表格demo'
+                    name: '表格demo',
+                    current: false
                 }]
             }
         },
         methods: {
-            clickToCurrent: function (e) {
-                console.log(e)
+            clickToCurrent: function (item) {
+                console.log(item.target.hash)
+                var current = item.target.hash
+                var currentMenus = this.menus
+                $.each(this.menus, function (index, menu) {
+                    if (current.indexOf(menu.path) > 0) {
+                        currentMenus[index].current = true
+                    } else {
+                        currentMenus[index].current = false
+                    }
+                })
+                this.menus = currentMenus
             }
         }
     }
